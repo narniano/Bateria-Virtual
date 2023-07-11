a3ef4619108247b33ee7511f23c889dd61f198e8
Rilson Joás <107469259+narniano@users.noreply.github.com>
2023-07-11 14:34:08 -0300

    Melhoria dos sons e do visual
    Melhoria dos sons e do visual da página. Alguns arquivos de som e de imagem foram acrescentados para uma nova atualização da página no futuro.

--- index.js
+++ index.js
@@ -19,27 +19,27 @@ document.addEventListener("keydown", function (event) {
 
 function makeSound(key) {
   switch (key) {
-    case "w":
+    case "y":
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
-    case "a":
-      var kickBass = new Audio("sounds/kick-bass.mp3");
+    case "x":
+      var kickBass = new Audio("sounds/bombo.wav");
       kickBass.play();
       break;
     case "s":
       var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
-    case "d":
+    case "g":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
-    case "j":
+    case "h":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
-    case "k":
+    case "j":
       var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
